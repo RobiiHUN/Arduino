880cb9bd83403890ba8baa51244e3f71e454a507
Robikagameplay <58343667+Robikagameplay@users.noreply.github.com>
2019-11-29 20:47:37 +0100

    Add files via upload

---
+++ sketch_oct19a.ino
@@ -0,0 +1,42 @@
+int pbuttonPin = 2;// connect output to push button
+int relayPin = 10;// Connected to relay (LED)
+
+int val = 0; // push value from pin 2
+int lightON = 0;//light status
+int pushed = 0;//push status
+
+
+void setup() {
+  // Robojax.com code and video tutorial for push button ON and OFF
+  Serial.begin(9600);
+  pinMode(pbuttonPin, INPUT_PULLUP); 
+  pinMode(relayPin, OUTPUT);
+ digitalWrite(relayPin, HIGH);// keep the load OFF at the begining. If you wanted to be ON, change the HIGH to LOW
+}
+
+void loop() {
+// Robojax.com code and video tutorial for push button ON and OFF
+  val = digitalRead(pbuttonPin);// read the push button value
+
+  if(val == HIGH && lightON == LOW){
+
+    pushed = 1-pushed;
+    delay(100);
+  }    
+
+  lightON = val;
+
+      if(pushed == HIGH){
+        Serial.println("Light ON");
+        digitalWrite(relayPin, LOW); 
+       
+      }else{
+        Serial.println("Light OFF");
+        digitalWrite(relayPin, HIGH);
+   
+      }     
+
+// Robojax.com code and video tutorial for push button ON and OFF
+
+  delay(100);
+}
